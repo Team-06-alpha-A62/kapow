643317dc4b03d3f737915d104375a02e939fa465
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-10 19:07:35 +0300

    fixed issue with url

--- template/src/services/request-service.js
+++ template/src/services/request-service.js
@@ -5,6 +5,6 @@ const API_KEY = 'FeFboweua5M9OT2rymbqjhh8DVsXxvxJ';
  * @return {Promise}
  */
 export const loadTrending = async () => {
-  const response = await fetch(`api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`);
+  const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`);
   return response.json();
 }
