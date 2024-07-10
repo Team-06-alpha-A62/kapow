24199997ca26a07fcb3e1bc7f6a9cc6d5fe5d19b
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-10 18:21:48 +0300

    implemented constants.js

---
+++ template/src/common/constants.js
@@ -0,0 +1,11 @@
+export const HOME = 'home';
+
+export const TRENDING = 'trending';
+
+export const FAVORITES = 'favorites';
+
+export const UPLOADED = 'uploaded';
+
+export const UPLOAD = 'upload';
+
+export const CONTAINER_SELECTOR = '#container';
