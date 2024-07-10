265b5fdbd004e6be9681ebb1491c830ec382a062
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-10 19:11:54 +0300

    Updated loadTrending to return the data array directly

--- template/src/services/request-service.js
+++ template/src/services/request-service.js
@@ -6,5 +6,6 @@ const API_KEY = 'FeFboweua5M9OT2rymbqjhh8DVsXxvxJ';
  */
 export const loadTrending = async () => {
   const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`);
-  return response.json();
+  const trending = response.json();
+  return trending.data;
 }
