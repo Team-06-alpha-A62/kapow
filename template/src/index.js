a88f1ceb855cc72b442a030fb82b2b08aaca95f8
Maikul <mcjaiyo01@gmail.com>
2024-07-10 18:03:33 +0300

    Implemented click handler for nav-link elements

--- template/src/index.js
+++ template/src/index.js
@@ -1,1 +1,9 @@
-console.log('hi');
+import { loadPage } from './events/navigation-events.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', event => {
+    if (event.target.classList.contains('nav-link')) {
+      loadPage(event.target.getAttribute('data-page'));
+    }
+  });
+});
