6a1888ef5080b425ab317ae0d85d878da81be989
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-11 00:17:57 +0300

    renderSearchGifs implementation

---
+++ template/src/events/search-events.js
@@ -0,0 +1,11 @@
+import { CONTAINER_SELECTOR } from '../common/constants.js';
+import { loadSearchGifs } from '../services/request-service.js';
+import { toSearchGifView } from '../views/search-vew.js';
+
+export const renderSearchGifs = async searchTerm => {
+  const gifs = await loadSearchGifs(searchTerm);
+  document.querySelector(CONTAINER_SELECTOR).innerHTML = toSearchGifView(
+    gifs,
+    searchTerm
+  );
+};
