841720f531262b21c3abecc6ce4a9b6780488544
Maikul <mcjaiyo01@gmail.com>
2024-07-10 19:16:25 +0300

    AWAIT

--- template/src/services/request-service.js
+++ template/src/services/request-service.js
@@ -5,7 +5,9 @@ const API_KEY = 'FeFboweua5M9OT2rymbqjhh8DVsXxvxJ';
  * @return {Promise}
  */
 export const loadTrending = async () => {
-  const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`);
-  const trending = response.json();
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
+  );
+  const trending = await response.json();
   return trending.data;
-}
+};
