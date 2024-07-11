fcad65dc018e25bdeed9ce7b4c2f3432a240fb0e
Maikul <mcjaiyo01@gmail.com>
2024-07-11 22:53:21 +0300

    added limit on requests

--- template/src/services/request-service.js
+++ template/src/services/request-service.js
@@ -6,7 +6,7 @@ const API_KEY = 'eb7zm6ySTzlB7D1yeWX7Zu6KJDECrPPM';
  */
 export const loadTrending = async () => {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
+    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=20`
   );
   const { data } = await response.json();
   return data;
@@ -22,7 +22,7 @@ export const loadSingleGif = async id => {
 
 export const loadSearchGifs = async (searchTerm = '') => {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}`
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=20`
   );
   const { data } = await response.json();
   return data;
