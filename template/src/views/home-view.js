f4e2f74f621a71293b8d17803beb29c8b99c5868
Nikola Nenovski <nicknenovski@gmail.com>
2024-07-16 00:15:34 +0300

    updated home view text

--- template/src/views/home-view.js
+++ template/src/views/home-view.js
@@ -9,9 +9,10 @@ export const toHomeView = () => {
 
       <div class="home-container">
       <h2><img src="./assets/Deer.webp" alt="deer" style="width: 80px; height: 60px" /> viewer,</h2>
-      <p>We've poured endless hours into enduring pointless gifs, all so you can enjoy the ultimate app experience!</p>
+      <p>We've poured endless hours into enduring pointless gifs, all so you can enjoy the full app experience, not just the free trial!</p>
       <img src="./assets/giphy_enjoy.gif" alt="leonardo approve" style="height: 300px; width: 800px; border-radius: 10px" />
       <p>You're welcome!</p>
+      <p>Yours,<br>Maikul & Nikola</p>
       </div>
     </div>
   `;
